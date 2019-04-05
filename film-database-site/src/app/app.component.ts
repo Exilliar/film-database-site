fb16eb83657f50995d1551c1b1354586aee08265
Rory Webber <Rory.Webber@arup.com>
2019-04-05 13:36:07 +0100

    #8 made the table, it just doesn't show up yet

--- film-database-site/src/app/app.component.ts
+++ film-database-site/src/app/app.component.ts
@@ -1,5 +1,21 @@
 import { Component } from '@angular/core';
 
+export interface data {
+  position: number;
+  name: string;
+  length: number;
+  watched: boolean;
+}
+
+export const ELEMENT_DATA: data[] = [{position:1,name:'film 1',length:100,watched:true},
+                              {position:2,name:'film 2',length:100,watched:true},
+                              {position:3,name:'film 3',length:100,watched:true},
+                              {position:4,name:'film 4',length:100,watched:true},
+                              {position:5,name:'film 5',length:100,watched:true},
+                              {position:6,name:'film 6',length:100,watched:true}
+                            ];
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,10 +24,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'film-database-site';
 
-  data = [{name:'film 1',length:100,watched:'true'},
-          {name:'film 2',length:100,watched:'true'},
-          {name:'film 3',length:100,watched:'true'},
-          {name:'film 4',length:100,watched:'true'},
-          {name:'film 5',length:100,watched:'true'},
-          {name:'film 6',length:100,watched:'true'}]
+  dataSource = ELEMENT_DATA;
+
+  displayedColumns: string[] = ['position', 'name', 'length', 'watched?'];
 }
