73f7c411777521a0f5e2862279158900ebbbb7d6
Exilliar <rorywebber1000@gmail.com>
2019-04-06 23:27:06 +0100

    #7 created a basic api, just testing that one can actually be curled

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,13 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
+app.get('/api/getData', (req,res) => {
+  res.status(200).send({
+    success: 'true',
+    message: 'this was successfull'
+  })
+})
+
 app.listen(8000, () => {
   console.log('Example app listening on port 8000!')
 });
