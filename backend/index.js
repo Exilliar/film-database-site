f01cf8ee79506b494133520ff06a1beb820d6301
Exilliar <rorywebber1000@gmail.com>
2019-04-07 01:40:44 +0100

    #12 connected it to the database, need to do some stuff with promises tomorrow

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,18 @@ const app = express();
 
 const cors = require('cors');
 
+var pgp = require('pg-promise')(/*options*/);
+var db = pgp('postgres://filmdatabase:' + process.env.POSTGRES_PASSOWRD + '@film-database.ciyl3ymdaics.eu-west-2.rds.amazonaws.com:5432/filmdatabase');
+
+// const Pool = require('pg').Pool
+// const pool = new Pool({
+//   user: 'filmdatabase',
+//   host: 'film-database.ciyl3ymdaics.eu-west-2.rds.amazonaws.com',
+//   database: 'filmdatabase',
+//   password: process.env.POSTGRES_PASSWORD,
+//   port: 5432
+// })
+
 var corsOptions = {
   origin: 'http://localhost:4200',
   optionsSuccessStatus: 200
@@ -15,27 +27,34 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/getData', (req,res) => {
-  res.status(200).send([{
-    position: 1,
-    name: 'film 1',
-    length: 100,
-    watched: true
-  },{
-    position: 2,
-    name: 'film 2',
-    length: 100,
-    watched: true
-  },{
-    position: 3,
-    name: 'film 3',
-    length: 100,
-    watched: true
-  },{
-    position: 4,
-    name: 'film 4',
-    length: 100,
-    watched: true
-  }])
+  let database = db.any('SELECT * FROM blurays');
+  console.log(database);
+
+
+  res.status(200).send(
+    database
+  // [{
+  //   position: 1,
+  //   name: 'film 1',
+  //   length: 100,
+  //   watched: true
+  // },{
+  //   position: 2,
+  //   name: 'film 2',
+  //   length: 100,
+  //   watched: true
+  // },{
+  //   position: 3,
+  //   name: 'film 3',
+  //   length: 100,
+  //   watched: true
+  // },{
+  //   position: 4,
+  //   name: 'film 4',
+  //   length: 100,
+  //   watched: true
+  // }]
+  )
 })
 
 app.listen(8000, () => {
