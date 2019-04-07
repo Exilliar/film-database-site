658a56acfd6cab21f23ba246986c363c899d2517
Exilliar <rorywebber1000@gmail.com>
2019-04-07 16:46:44 +0100

    #12 changes to the backend

--- backend/index.js
+++ backend/index.js
@@ -6,15 +6,6 @@ const cors = require('cors');
 var pgp = require('pg-promise')(/*options*/);
 var db = pgp('postgres://filmdatabase:' + process.env.POSTGRES_PASSOWRD + '@film-database.ciyl3ymdaics.eu-west-2.rds.amazonaws.com:5432/filmdatabase');
 
-// const Pool = require('pg').Pool
-// const pool = new Pool({
-//   user: 'filmdatabase',
-//   host: 'film-database.ciyl3ymdaics.eu-west-2.rds.amazonaws.com',
-//   database: 'filmdatabase',
-//   password: process.env.POSTGRES_PASSWORD,
-//   port: 5432
-// })
-
 function getAllData() {
   return new Promise((resolve,reject) => {
     resolve(db.any('SELECT * FROM blurays'));
@@ -38,8 +29,6 @@ app.get('/api/getData', (req,res) => {
     console.log(data);
     res.status(200).send(data);
   })
-  
-  
 })
 
 app.listen(8000, () => {
