a87e39e25eb9e7a721e2e9dac3569ce8b294e3ae
Rory Webber <Rory.Webber@arup.com>
2019-04-12 17:48:09 +0100

    #14 added routing so that the header can work better

--- film-database-site/src/app/app-routing.module.ts
+++ film-database-site/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BluraysComponent } from './pages/blurays/blurays.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'blurays',
+    pathMatch: 'full'
+  }, {
+    path: 'blurays',
+    component: BluraysComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
