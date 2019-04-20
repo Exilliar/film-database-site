82dba7befa7793e24d38c9c968f6c0ed1c6948f9
Exilliar <rorywebber1000@gmail.com>
2019-04-20 22:17:51 +0100

    #17 added some more imports to app.module

--- film-database-site/src/app/app.module.ts
+++ film-database-site/src/app/app.module.ts
@@ -6,12 +6,22 @@ import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
 
+// Material modules
 import { MaterialModule } from './material.module';
 
 import {CdkTableModule} from '@angular/cdk/table';
+
+// Page components
 import { BluraysComponent } from './pages/blurays/blurays.component';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
+
+// Firebase authentication
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
 
 @NgModule({
   declarations: [
@@ -24,7 +34,10 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     CdkTableModule,
     HttpClientModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule // imports firebase/auth, only needed for auth features
   ],
   providers: [],
   bootstrap: [AppComponent]
