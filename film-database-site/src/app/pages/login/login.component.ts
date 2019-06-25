c3f8187a66a5d655b22fb5e4d8e12c7064034933
Rory Webber <Rory.Webber@arup.com>
2019-06-25 17:54:29 +0100

    #49 issue fixed, also removed some unused imports from login component

--- film-database-site/src/app/pages/login/login.component.ts
+++ film-database-site/src/app/pages/login/login.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component  } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
-import { Router, Params } from '@angular/router';
+import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../../auth/user.service';
 
